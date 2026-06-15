9a1f9e8479c270d54c5b981f9833117ef3746199
Uttkarshchambiyal <uttkarshchambiyal26@gmail.com>
2026-06-15 21:26:14 +0530

    Add transaction feature

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+/* Push 4 — Uttkarsh | Add transaction logic */
+const form = document.getElementById("transaction-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const desc = document.getElementById("desc").value;
+  const amount = parseFloat(document.getElementById("amount").value);
+  const type = document.getElementById("type").value;
+
+  transactions.push({ desc, amount, type });
+
+  saveData();
+  renderTransactions();
+  form.reset();
+});
