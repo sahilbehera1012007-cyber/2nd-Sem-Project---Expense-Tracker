0960107172d353cce62d58772fcdd9f741ee6a4a
Abhishek deshmukh <abhishek19072006@gmail.com>
2026-06-16 14:16:45 +0530

    Add event listener for transaction form submission / Add Transaction

--- script.js
+++ script.js
@@ -83,3 +83,36 @@ function deleteTransaction(index) {
     saveData();
     renderTransactions();
 }
+
+
+
+//abhi
+const form =
+document.getElementById("transaction-form");
+
+form.addEventListener("submit", (e) => {
+
+  e.preventDefault();
+
+  const desc =
+  document.getElementById("desc").value;
+
+  const amount =
+  parseFloat(
+    document.getElementById("amount").value
+  );
+
+  const type =
+  document.getElementById("type").value;
+
+  transactions.push({
+    desc,
+    amount,
+    type
+  });
+
+  saveData();
+  renderTransactions();
+
+  form.reset();
+});
